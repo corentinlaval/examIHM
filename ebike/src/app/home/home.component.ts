5931da28db094bde22c52cdca539ad999086a393
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-01-28 16:59:52 +0100

    end

--- ebike/src/app/home/home.component.ts
+++ ebike/src/app/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Lottie from 'lottie-web';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+  bikeAnimationPath = 'assets/bike-animation.json';
+  animation: any;
 
+  ngOnInit(): void {
+    // Code d'initialisation si nécessaire
+  }
+
+  ngAfterViewInit(): void {
+    const lottieContainer = document.getElementById('lottie');
+    if (lottieContainer) {
+      this.animation = Lottie.loadAnimation({
+        container: lottieContainer,
+        path: this.bikeAnimationPath,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+      });
+    } else {
+      console.error('Lottie container not found!');
+    }
+  }
 }
